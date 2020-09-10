87f836cc7c91712b0892bf33427c5dced1e812df
Meera Jose Mampilly <meerajm@gmail.com>
2020-09-10 11:45:40 +0200

    first draft

---
+++ src/Actions.js
@@ -0,0 +1,224 @@
+import React, { useState } from "react";
+// read: https://fontawesome.com/how-to-use/on-the-web/using-with/react
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import data from './data.json'
+
+
+const Actions = (props) => {
+  const {setRestaurants}=props;
+
+  const [showSortSelector, setSortSelector] = useState(false);
+  const [showDietarySelector, setDietarySelector] = useState(false);
+  const [showPriceRangeSelector, setPriceRangeSelector] = useState(false);
+  const [dietTypeArr,setDietTypeArr] = useState("");
+
+  const handeGeneralSorting = (restaurants, sortBy) => {    
+    let sortedRestaurants = [];
+    if (sortBy === "maxDeliveryTime") {
+      sortedRestaurants = restaurants
+        .slice()
+        .sort((a, b) => a[sortBy] - b[sortBy]);
+    } else {
+      sortedRestaurants = restaurants
+        .slice()
+        .sort((a, b) => b[sortBy] - a[sortBy]);
+    }  
+    console.log(sortedRestaurants);
+    document.getElementById("printMsg").innerHTML="Restaurants sorted in the order of "+sortBy;
+    setRestaurants(sortedRestaurants);
+  };
+
+  let priceRangeSelect = (restaurants, minPrice, maxPrice = 1000) => {
+    let priceRangeRestaurants = [];
+    restaurants.forEach((restaurantElement) => {
+      restaurantElement.menu.forEach((menuElement) => {
+        menuElement.items.forEach((itemsElement) => {
+          if (
+            itemsElement.price >= minPrice &&
+            itemsElement.price <= maxPrice
+          ) {
+            priceRangeRestaurants.push(restaurantElement);
+          }
+        });
+      });
+    });
+    priceRangeRestaurants = [...new Set(priceRangeRestaurants)]; // removing the duplicate entries of restaurants
+    console.log(restaurants);
+    console.log(priceRangeRestaurants);
+    document.getElementById("printMsg").innerHTML="You have selected price range between $"+minPrice+" and $"+maxPrice;
+    setRestaurants(priceRangeRestaurants);
+  };
+
+  let getDietTypes=(diet,isDietChecked)=>{
+let dietTypeArrCopy=[];
+isDietChecked
+? dietTypeArrCopy.push(diet)
+: dietTypeArrCopy.splice(dietTypeArrCopy.indexOf(diet), 1);
+setDietTypeArr(dietTypeArrCopy);
+console.log(dietTypeArrCopy);
+  }
+
+  let dietarySelect = (restaurants,dietTypeArr) => {
+    let dietSelected = [];
+    //Add all the selected dietary to an array(dietTypeArr)
+    
+    restaurants.forEach((element) => {
+      element.menu.forEach((itemsElement) => {
+        itemsElement.items.forEach((data) => {
+          dietTypeArr.forEach((diet) => {
+            if (data.typeOfMeal === diet) {
+              dietSelected.push(element);
+            }
+          });
+        });
+      });
+    });
+    dietSelected = [...new Set(dietSelected)]; // removing the duplicate entries of restaurants
+    console.log(dietSelected);
+    document.getElementById("printMsg").innerHTML=" ";
+    setRestaurants(dietSelected);
+  };
+
+  return (
+    <div className="actions">
+      <span className="button-group">
+        <button onClick={() => {setPriceRangeSelector(false);setDietarySelector(false); setSortSelector(!showSortSelector)}}>
+          <span>Sort</span> <FontAwesomeIcon icon={"chevron-down"} />
+        </button>
+        {showSortSelector && (
+          <div>
+            <label>
+              Most popular{" "}
+              <input
+                value="popularity"
+                type="radio"
+                name="generalSort"
+                onChange={(event) =>
+                  handeGeneralSorting(
+                    data.restarants,
+                    event.currentTarget.value
+                  )
+                }
+              />
+            </label>
+            <label>
+              Rating{" "}
+              <input
+                value="rating"
+                type="radio"
+                name="generalSort"
+                onChange={(event) =>
+                  handeGeneralSorting(
+                    data.restarants,
+                    event.currentTarget.value
+                  )
+                }
+              />
+            </label>
+            <label>
+              Delivery time{" "}
+              <input
+                value="maxDeliveryTime"
+                type="radio"
+                name="generalSort"
+                onChange={(event) =>
+                  handeGeneralSorting(
+                    data.restarants,
+                    event.currentTarget.value
+                  )
+                }
+              />
+            </label>
+          </div>
+        )}
+      </span>
+      {/* TODO */}
+      {/* <!-- Implement as assignment for Thursday --> */}
+      <span className="button-group">
+        <button onClick={() =>{setDietarySelector(false);setSortSelector(false); setPriceRangeSelector(!showPriceRangeSelector)}}>
+          <span>Price Range</span> <FontAwesomeIcon icon={"chevron-down"} />
+        </button>
+        {showPriceRangeSelector && (
+          <div>
+            <button
+              onClick={(event) => priceRangeSelect(data.restarants, 50, 100)}
+            >
+              <span>$50-$100</span>
+            </button>
+            <button
+              onClick={(event) => priceRangeSelect(data.restarants, 101, 200)}
+            >
+              <span>$101-$200</span>
+            </button>
+            <button
+              onClick={(event) => priceRangeSelect(data.restarants, 201, 300)}
+            >
+              <span>$201-$300</span>
+            </button>
+            <button
+              onClick={(event) => priceRangeSelect(data.restarants, 301)}
+            >
+              <span>Above $300</span>
+            </button>
+          </div>
+        )}
+      </span>
+      <span className="button-group">
+        <button onClick={() => {setSortSelector(false);setPriceRangeSelector(false);setDietarySelector(!showDietarySelector)}}>
+          <span>Dietary choice</span> <FontAwesomeIcon icon={"chevron-down"} />
+        </button>
+        {showDietarySelector && (
+          <div>
+            <label>
+              Vegetarian{" "}
+              <input
+                value="vegetarian"
+                type="checkbox"
+                id="cb1"
+                name="dietaryFilter"
+                onChange={(event) =>
+                  getDietTypes(
+                    event.currentTarget.value,
+                    event.currentTarget.checked
+                  )
+                }
+              />
+            </label>
+            <label>
+              Vegan{" "}
+              <input
+                value="vegan"
+                type="checkbox"
+                id="cb1"
+                name="dietaryFilter"
+                onChange={(event) =>
+                  getDietTypes(
+                    event.currentTarget.value,
+                    event.currentTarget.checked
+                  )
+                }
+              />
+            </label>
+            <label>
+              Non-vegan{" "}
+              <input
+                value="non vegan"
+                type="checkbox"
+                id="cb1"
+                name="dietaryFilter"
+                onChange={(event) =>
+                  getDietTypes(
+                    event.currentTarget.value,
+                    event.currentTarget.checked
+                  )
+                }
+              />
+            </label>
+            <button onClick={()=>dietarySelect(data.restarants,dietTypeArr)}>Show restaurants</button>
+          </div>
+        )}
+      </span>
+    </div>
+  );
+};
+export default Actions;
